4b7f3dfb13e70b72266ee94a81bc9482e34a0e7f
PV Anfas <anfaspv.info@gmail.com>
2023-04-26 20:01:17 +0530

    up

--- jamiasalafiya/web/admin.py
+++ jamiasalafiya/web/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Result
 
 
-admin.site.register(Result)
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ("title", "file", "is_active")
+    list_filter = ("is_active",)
+    list_editable = ("is_active",)
+    search_fields = ("title",)
